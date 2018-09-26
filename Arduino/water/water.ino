2c3194cc4f5e7371fa2edd90be0289b7fae9f122
luojilashi <luojilashi@hotmail.com>
2018-09-27 00:36:59 +0800

    水柜控制

--- Arduino/water/water.ino
+++ Arduino/water/water.ino
@@ -25,9 +25,13 @@ Servo myservo;        // 创建一个舵机对象
 #define MID_GRADE_2		1600
 #define HIGH_GRADE_1	1900
 #define HIGH_GRADE_2	2100
-
+int checkTime = 600;
+unsigned long checkFullTime = 0;
+unsigned long checkEmptyTime = 0;
 const int intControl = 5;//声音信号
 const int outControl = 6;//主炮信号
+int fullState = 0;
+int emptyState = 0;
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
@@ -35,65 +39,54 @@ void setup()
 	pinMode(intControl, INPUT);
 	myservo.attach(outControl);
 	myservo.writeMicroseconds(MID_GRADE);
+	checkFullTime = millis();
+	checkEmptyTime = millis();
 }
 
 // Add the main program code into the continuous loop() function
 void loop()
 {
-	int control = pulseIn(10, HIGH);
-	int hi = analogRead(A0);
-	int gi = analogRead(A1);
-	Serial.print("A0:");
-	Serial.print(hi);
-	Serial.print("  A1:");
-	Serial.println(control);
-
-	if (control<2050&&control > 1600)
+	int cState = getState();
+	switch (cState)
 	{
-		if(hi<200)
-		{
-			myservo.writeMicroseconds(1070);
-		}
+	case 1://上浮 排水
+		if (getWaterFull())
+			setPump();
 		else
-		{
-			myservo.writeMicroseconds(1500);
-		}
+			setPump(cState);
+		break;
+	case 2://下潜 进水 
+		if (getWaterEmpty())
+			setPump();
+		else
+			setPump(cState);
+		break;
+	default:
+		setPump();
+		break;
 	}
-	else if (control < LOW_GRADE_2 && control>LOW_GRADE_1)
-	{
-		if (myservo.readMicroseconds() < 1900)
-		{
-			myservo.writeMicroseconds(1930);
-			delay(100);
-			myservo.writeMicroseconds(1500);
-			delay(100);
-			myservo.writeMicroseconds(1930);
-		}		
-	}
-	else if(control<MID_GRADE_2 && control>MID_GRADE_1)
-	{
-		myservo.writeMicroseconds(1500);
-	}
+	setPump(1);
 
-	delay(50);
-
+	delay(5);
 }
 
-int getStart()
+int getState()
 {
-	delay(10);
+	delay(5);
 	int control = pulseIn(intControl, HIGH);
 	if (control > LOW_GRADE_1 && control < LOW_GRADE_2)
 	{
+		//上浮 排水
 		return 1;
 	}
 	else if (control > MID_GRADE_1 && control < MID_GRADE_2)
 	{
-		return 2;
+		return 0;
 	}
 	else if (control > HIGH_GRADE_1 && control < HIGH_GRADE_2)
 	{
-		return 3;
+		//下潜 进水 
+		return 2;
 	}
 	else
 	{
@@ -101,3 +94,97 @@ int getStart()
 	}
 }
 
+void setPump(int pumpState = 0)
+{
+	switch (pumpState)
+	{
+	case 0:
+		myservo.writeMicroseconds(MID_GRADE);
+		break;
+	case 1://排水
+		myservo.writeMicroseconds(LOW_GRADE_2);
+		break;
+	case 2://进水
+		myservo.writeMicroseconds(HIGH_GRADE_1);
+		delay(10);
+		myservo.writeMicroseconds(MID_GRADE);
+		delay(10);
+		myservo.writeMicroseconds(HIGH_GRADE_1);
+		break;
+	default:
+		myservo.writeMicroseconds(MID_GRADE);
+		break;
+	}
+}
+
+int getWaterFull()
+{
+	auto mills = millis();
+	int hi = analogRead(A0);
+
+	switch (fullState)
+	{
+	case 1://full
+		if (hi < 200 && (mills - checkFullTime)>checkTime)
+		{
+			fullState=0;
+			checkFullTime = mills;
+		}
+		else if (hi > 200)
+		{
+			checkFullTime = mills;
+		}
+		break;
+	case 0:
+		if (hi > 200 && (mills - checkFullTime)>checkTime)
+		{
+			fullState = 1;
+			checkFullTime = mills;
+		}
+		else if (hi < 200)
+		{
+			checkFullTime = mills;
+		}
+		break;
+	default:
+		break;
+	}
+
+	return fullState;
+}
+
+int getWaterEmpty()
+{
+	auto mills = millis();
+	int hi = analogRead(A1);
+
+	switch (emptyState)
+	{
+	case 1://empty
+		if (hi < 200 && (mills - checkEmptyTime)>checkTime)
+		{
+			emptyState = 0;
+			checkEmptyTime = mills;
+		}
+		else if (hi > 200)
+		{
+			checkEmptyTime = mills;
+		}
+		break;
+	case 0:
+		if (hi > 200 && (mills - checkEmptyTime)>checkTime)
+		{
+			emptyState = 1;
+			checkEmptyTime = mills;
+		}
+		else if (hi > 200)
+		{
+			checkEmptyTime = mills;
+		}
+		break;
+	default:
+		break;
+	}
+
+	return emptyState;
+}
